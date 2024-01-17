fd7d6188cae64b1150c5e5cafe397a23808dfbe1
hnahk02 <100854712+hnahk02@users.noreply.github.com>
2024-01-17 23:42:22 +0700

    get config in appsettings.json

--- BusinessObject/Model/eStoreContext.cs
+++ BusinessObject/Model/eStoreContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace BusinessObject.Model
 {
@@ -26,8 +27,7 @@ namespace BusinessObject.Model
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server =khanh; database = eStore;uid=hank02;pwd=khanhoc2002;");
+                optionsBuilder.UseSqlServer(new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("MyDB"));
             }
         }
 
